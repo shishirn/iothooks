f888ce6de47312b59de074555ca90907ef9b4bfd
shishirnegi <shishirnegi22@gmail.com>
2022-02-27 21:26:48 -0500

    First commit

---
+++ src/main/java/com/shishir/iothooks/IothooksApplication.java
@@ -0,0 +1,13 @@
+package com.shishir.iothooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IothooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IothooksApplication.class, args);
+	}
+
+}
