9e2ea49e3c90fe27676f0052d475abd8b65185c6
shishirnegi <shishirnegi22@gmail.com>
2022-02-27 21:59:53 -0500

    added TempSensor model class

---
+++ src/main/java/com/shishir/iothooks/models/TempSensorModel.java
@@ -0,0 +1,53 @@
+package com.shishir.iothooks.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Date;
+
+@Entity
+public class TempSensorModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Date dateTime;
+    private Float temperature;
+    // only Celsius and Fahrenheit
+    private String unit;
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(Date dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public Float getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(Float temperature) {
+        this.temperature = temperature;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+}
