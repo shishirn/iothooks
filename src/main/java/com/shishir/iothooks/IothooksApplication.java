c65a420d42c7b53dc6e725f2289c1971e1d4d406
shishirnegi <shishirnegi22@gmail.com>
2022-02-27 21:39:39 -0500

    added project structure

--- src/main/java/com/shishir/iothooks/IothooksApplication.java
+++ src/main/java/com/shishir/iothooks/IothooksApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IothooksApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(IothooksApplication.class, args);
 	}
-
 }
